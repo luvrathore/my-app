4b210fa95df6e89406a33eb6a832daf8e9fcd02c
luvrathore <jrjatinrathore@gmail.com>
2022-10-26 00:08:26 +0530

    delete file in practiceComponents

--- src/practiceComponents/SlugComponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function SlugComponent(props) {
-  return <h1>{" We are playing " + props.match.params.slug}</h1>;
-}
-
-export default SlugComponent;
--- src/practiceComponents/VideoPage.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function VideoPage(){
-
-  return <h1>Hello Video Page </h1>;
-}
-
-export default VideoPage;
